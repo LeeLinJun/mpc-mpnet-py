d4b71259ea9ca364ef5f69d830dbe3cd1c2dd89d
arclabdl1 <you@example.com>
2020-04-30 01:59:37 -0700

    implemented tree based

--- benchmark.py
+++ benchmark.py
@@ -8,70 +8,25 @@ import pickle
 import time
 import click
 
+from params.step5_s1024_e32 import get_params
 
-def get_params(final_goal):
-    n_t = 5
-    state_dim = 4
-    control_dim = 1
-    weights = np.ones(state_dim)*1
-    weights[2:] = 0.25
-    n_sample = 1024
-    n_elite = 32
-    t = 1e-1
-    dt = 2e-2
-
-    mu_t, sigma_t = 1e-1, 4e-1
-    t_min, t_max = 0, 5e-1
-
-    mu_u = np.zeros((n_t*control_dim))
-    sigma_u_diag = np.ones(n_t*control_dim)
-    sigma_u_diag[:] = 4
-    sigma_u = np.diag(sigma_u_diag)
-    params = {
-        'n_sample': n_sample,
-        'n_elite': n_elite,
-        'n_t': n_t,
-        'weights': weights,
-        'mu_u': mu_u,
-        'sigma_u': sigma_u,
-        't': t,
-        'dt': dt,
-
-        'mu_t': np.ones(n_t) * mu_t,
-        'sigma_t': np.identity(n_t)*sigma_t,
-        't_min': t_min,
-        't_max': t_max,
-
-        'state_dim': state_dim,
-        'control_dim': control_dim,
-        'converge_radius': 1e-2,
-        'drop_radius': 1,
-        'goal_radius': 10, #np.sqrt(2),
-        'max_it': 20,
-        'rolling_count': n_t,
-        'bk_it': 2,
-        'final_goal': final_goal, #ref_path[-1], #np.array([np.inf, np.inf, np.inf, np.inf]), #
-        'mpc_mode': 'solve',
-    #     'mpc_mode': 'rolling',
-    }
-    return params
-    
-def experiment(env_id, traj_id, verbose=False, max_it=300):
+def experiment(env_id, traj_id, verbose=False):
     obs_list = get_obs('acrobot_obs', env_id)
     data = load_data('acrobot_obs', env_id, traj_id)
     ref_path = data['path']
     env_vox = torch.from_numpy(np.load('mpnet/sst_envs/acrobot_obs_env_vox.npy')[env_id]).unsqueeze(0).float()
     
     tic = time.perf_counter()
-
+    
     ## initiate planner
+    params = get_params(ref_path[-1])
     mpc_mpnet = MPCMPNetPlanner(
-        get_params(ref_path[-1]),
+        params,
         ref_path[0], 
         ref_path[-1],
         env_vox,
         system="acrobot_obs",
-        #setup="default_norm",
+#         setup="default_norm",
         #setup="default_norm_aug",
         setup="norm_nodiff_noaug_20step2e-2",
         ep=5000,
@@ -81,12 +36,12 @@ def experiment(env_id, traj_id, verbose=False, max_it=300):
     
     ## start experiment
     it = 0
-    while it < max_it and not mpc_mpnet.reached:
+    while it < params['max_plan_it'] and not mpc_mpnet.reached:
         mpc_mpnet.reset()
-        for i in range(50):
+        for i in range(20):
             if verbose:
                 print('iteration:', i)
-            mpc_mpnet.plan()
+            mpc_mpnet.step()
             if mpc_mpnet.reached:
                 break
             it += 1
@@ -111,7 +66,7 @@ def experiment(env_id, traj_id, verbose=False, max_it=300):
     return result
     
 
-def full_benchmark(num_env, num_traj):
+def full_benchmark(num_env, num_traj, save=True, config='default'):
     sr = np.zeros((num_env, num_traj))
     time = np.zeros((num_env, num_traj))
     costs = np.zeros((num_env, num_traj))
@@ -123,9 +78,10 @@ def full_benchmark(num_env, num_traj):
             if result['successful']:
                 time[env_id, traj_id] = result['planning_time']
                 costs[env_id, traj_id] = result['costs']
-            np.save('results/full/sr_{}_{}.npy'.format(num_env, num_traj), sr)
-            np.save('results/full/time_{}_{}.npy'.format(num_env, num_traj), time)
-            np.save('results/full/costs_{}_{}.npy'.format(num_env, num_traj), costs)
+            if save:
+                np.save('results/full/sr_{}_{}_{}.npy'.format(config, num_env, num_traj), sr)
+                np.save('results/full/time_{}_{}_{}.npy'.format(config, num_env, num_traj), time)
+                np.save('results/full/costs_{}_{}_{}.npy'.format(config, num_env, num_traj), costs)
 
 
 @click.command()
@@ -134,11 +90,13 @@ def full_benchmark(num_env, num_traj):
 @click.option('--traj_id', default=0)
 @click.option('--num_env', default=10)
 @click.option('--num_traj', default=1000)
-def main(full, env_id, traj_id, num_env, num_traj):
+@click.option('--save', default=True)
+@click.option('--config', default='ls')
+def main(full, env_id, traj_id, num_env, num_traj, save, config):
     if not full:
         result = experiment(env_id, traj_id)
     else:
-        result = full_benchmark(num_env, num_traj)
+        result = full_benchmark(num_env, num_traj, save, config)
    
     
     
--- scripts/mpcmpnet_linesearch.py
+++ scripts/mpcmpnet_linesearch.py
@@ -6,11 +6,6 @@ from mpnet.normalizer import Normalizer
 from mpc.cem_mpc_teb import MPC
 from mpc.systems.acrobot_vec_tv import Acrobot
 
-import sys
-sys.path.append('/media/arclabdl1/HD1/Linjun/mpc-mpnet-py/deps/sparse_rrt-1')
-from sparse_rrt import _sst_module
-from sparse_rrt.systems import standard_cpp_systems
-
 class MPCMPNetPlanner:
     def __init__(self,
                  params,
@@ -19,10 +14,9 @@ class MPCMPNetPlanner:
                  env_vox,
                  system="acrobot_obs",
                  setup="norm_nodiff_noaug_20step2e-2",
-                 ep=5000,
+                 ep=5,
                  obs_list=None,
-                 verbose=False,
-                 distance_func=None
+                 verbose=False
                 ):
         self.verbose = verbose
         self.system = system
@@ -41,43 +35,21 @@ class MPCMPNetPlanner:
         self.start = start
         self.goal = np.expand_dims(goal, axis=0)
         self.env_vox = env_vox     
-        self.samples = []
-        self.path = []
-        self.distance_func = distance_func
         
         self.reset()
-        assert self.params['planning_mode'] in ['line_search','tree']
-        if self.params['planning_mode'] == 'tree':
-            self.reset_sst_backend()
+        
+        self.trials = []
         
     def reset(self):
         self.state = np.expand_dims(self.start, axis=0)
         self.bk_state = [self.state.copy()]
         self.bk_count = 0
-        if self.params['planning_mode'] == 'line_search':
-            self.samples = []
-            self.path = []
-
+        self.path = []
+        self.samples = []
         self.reached = False
         self.costs = []
-    
-    def reset_sst_backend(self):
-        if self.system == "acrobot_obs":
-            system = standard_cpp_systems.TwoLinkAcrobot()
- 
-        self.tree_backend = _sst_module.SSTBackendWrapper(
-            state_bounds=system.get_state_bounds(),
-            control_bounds=system.get_control_bounds(),
-            distance=system.distance_computer() if self.distance_func is None else self.distance_func,
-            start_state=self.start, 
-            goal_state=self.goal[0],
-            goal_radius=self.params['goal_radius'],
-            random_seed=0,
-            sst_delta_near=self.params['delta_near'],
-            sst_delta_drain=self.params['delta_drain']
-        )
-    
-    def sample_state(self, start, goal):
+        
+    def sample(self, start, goal):
         """sample a way point
         
         Arguments:
@@ -98,7 +70,7 @@ class MPCMPNetPlanner:
         sample = self.normalizer.denormalize(sample.numpy())
         return sample # in configration space
     
-    def steer(self, start, sample):
+    def steer(self, sample):
         """steer nodes with mpc
         
             Arguments:
@@ -112,23 +84,22 @@ class MPCMPNetPlanner:
                 else None
         """
         if self.params['mpc_mode'] == 'rolling':
-            best_x, min_loss, path, collision = self.mpc.rolling(start.copy(), sample, obs_list=self.obs_list)
+            best_x, min_loss, path, collision = self.mpc.rolling(self.state[0], sample, obs_list=self.obs_list)
         elif self.params['mpc_mode'] == 'solve':
-            best_x, min_loss, path, collision, cost, _ = self.mpc.solve(start.copy(), sample, obs_list=self.obs_list)
+            best_x, min_loss, path, collision, cost, _ = self.mpc.solve(self.state[0], sample, obs_list=self.obs_list)
 #         print(self.state[0])    
 #         print(path)
      
         if collision or min_loss > self.params['drop_radius']:
             if self.verbose:
                 print('\t drop', min_loss)
-            if self.params['planning_mode'] == 'line_search':
-                self.bk_count += 1
-                if self.bk_count > self.params['bk_it']:
-                    self.bk_count = 0
-    #                 self.reset()
-    #                 self.trials.append(self.path.copy())
-                    self.state = self.bk_state[-1]
-                    self.bk_state.pop()
+            self.bk_count += 1
+            if self.bk_count > self.params['bk_it']:
+                self.bk_count = 0
+#                 self.reset()
+#                 self.trials.append(self.path.copy())
+                self.state = self.bk_state[-1]
+                self.bk_state.pop()
             return [], [], False
         else:
             self.bk_count = 0
@@ -136,7 +107,7 @@ class MPCMPNetPlanner:
             self.bk_state += [self.state.copy()]
             return path, np.sum(np.reshape(cost, -1)), True # new state and path
         
-    def step_linesearch(self):
+    def plan(self):
         """plan if not converge, firstly sample a way point, then try to steer with mpc
            
            Arguments:
@@ -157,62 +128,18 @@ class MPCMPNetPlanner:
                 if self.verbose:
                     print(goal_distance)
             
-            if goal_distance > 20 and np.random.rand() > 0.5:
-#             if True:
-                sample = self.sample_state(self.state, self.goal)
+            #if goal_distance > 10:
+            if True:
+                sample = self.sample(self.state, self.goal)
                 self.samples.append(sample[0])
             else:
                 sample = self.goal
             ## pass sample in 1-dim [state_dim]
-            path_i, cost_i, success = self.steer(self.state[0], sample[0])
+            path_i, cost_i, success = self.steer(sample[0])
             if success:
                 self.path += path_i
                 self.costs += [cost_i]
-    
-    def step_tree(self):
-        """plan if not converge, firstly sample a way point, then try to steer with mpc
-           
-           Arguments:
-                None
-                
-           Returns:
-                None
-        """
-        goal_distance = self.dynamics.get_distance(self.state, self.goal[0], self.params['weights'])
-#             goal_distance = self.dynamics.get_loss(self.state, self.goal[0], self.params['weights'])
-        if self.verbose:
-            print(goal_distance)
-
-        solution = self.tree_backend.get_solution()
-        if solution is not None:
-            print('####reached####:')
-            self.reached = True
-            self.path = solution[0]
-            self.costs = solution[2]
-        else:
-            if self.params['tree_sample']:
-                random_state = (np.random.rand(4)-0.5) * 2 * np.array([np.pi, np.pi, 6, 6])
-                nearest = self.tree_backend.nearest_vertex(random_state.copy())
-                sample = self.sample_state(np.expand_dims(nearest, axis=0), self.goal)[0]
-            else:
-                sample = self.sample_state(self.state, self.goal)[0]
-                self.samples.append(sample)
-
-                nearest = self.tree_backend.nearest_vertex(sample.copy())
-
-
-            self.samples.append(sample[0])
-            path_i, cost_i, success = self.steer(nearest, sample)
-            if success:
-                self.tree_backend.add_to_tree(self.state[0].copy(), cost_i)
-                self.path += path_i
-                self.costs += [cost_i]
-    
-    def step(self):
-        if self.params['planning_mode'] == 'line_search':
-            self.step_linesearch()
-        elif self.params['planning_mode'] == 'tree':
-            self.step_tree()
+            
 #     def plan_waypoints(self):
 #         """plan if not converge, sample waypoints to form a path
            
